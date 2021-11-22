8a9ad4cf8ac2492c08f0e4ed46fb2353b839882d
chahzhik <94822408+chahzhik@users.noreply.github.com>
2021-11-22 11:10:06 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > 5 && num1 < 10)
+            {
+                Console.WriteLine("Число меньше 5 и больше 10");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число");
+            }
+            Console.ReadKey();
+        }
+    }
+}
